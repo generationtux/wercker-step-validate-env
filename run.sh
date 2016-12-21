5465bdf18cf2bcac223377d5e41efd0d881c124d
Wes Eklund <wes.eklund@generationtux.com>
2016-12-21 14:09:19 -0500

    modifying step that adheres to Par API

--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
 info "Validating application env config"
+
+if [ -z "$WERCKER_VALIDATE_ENV_FILES" ]
+then
+    fail 'missing files to compare environment variables against'
+fi
+
+if [ -z "$WERCKER_VALIDATE_ENV_ARGS" ]
+then
+    fail 'missing argments to correctly parse the environment variables'
+fi
+
+par $WERCKER_VALIDATE_ENV_ARGS $WERCKER_VALIDATE_ENV_ARGS
+
+success "par: env variables are consistent across environments"
